c8c34182c2e4b15253d82c7ff425781fb8bd7dcc
Krupkolllia <krupkoilla397@gmail.com>
2026-03-17 00:19:07 +0100

    feat: implement ShopTransaction

--- src/main/java/core/basesyntax/model/ShopTransaction.java
+++ src/main/java/core/basesyntax/model/ShopTransaction.java
@@ -1,10 +1,54 @@
 package core.basesyntax.model;
 
 public class ShopTransaction {
+    private Operation operation;
+    private String product;
+    private int quantity;
+
+    public ShopTransaction(Operation operation, String product, int quantity) {
+        this.operation = operation;
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    public Operation getOperation() {
+        return operation;
+    }
+
+    public void setOperation(Operation operation) {
+        this.operation = operation;
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
     public enum Operation {
-        BALANCE,
-        SUPPLY,
-        PURCHASE,
-        RETURN
+        BALANCE("b"),
+        SUPPLY("s"),
+        PURCHASE("p"),
+        RETURN("r");
+
+        private final String code;
+
+        Operation(String code) {
+            this.code = code;
+        }
+
+        public String getCode() {
+            return code;
+        }
     }
 }
