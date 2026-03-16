2da95cafe04f885f73d46aa41d57f03ce2377c85
Krupkolllia <krupkoilla397@gmail.com>
2026-03-16 23:43:25 +0100

    refactor: make ReportGenerator generic

--- src/main/java/core/basesyntax/util/impl/CsvShopReportGenerator.java
+++ src/main/java/core/basesyntax/util/impl/CsvShopReportGenerator.java
@@ -3,8 +3,8 @@ package core.basesyntax.util.impl;
 import core.basesyntax.db.Storage;
 import core.basesyntax.util.ReportGenerator;
 
-public class CsvReportGenerator implements ReportGenerator {
-    public String generate(Storage source) {
+public class CsvShopReportGenerator implements ReportGenerator<String, Integer> {
+    public String generate(Storage<String, Integer> source) {
         return "";
     }
 }
