b151cd63137b34c2d19a5df56048e35d94a4722c
Krupkolllia <krupkoilla397@gmail.com>
2026-03-16 23:57:28 +0100

    feat: implement CsvFileReader

--- src/main/java/core/basesyntax/db/impl/CsvFileReader.java
+++ src/main/java/core/basesyntax/db/impl/CsvFileReader.java
@@ -1,12 +1,24 @@
 package core.basesyntax.db.impl;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import core.basesyntax.db.FileReader;
-
+import java.io.IOException;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CsvFileReader implements FileReader {
     @Override
     public List<String> read(String filePath) {
-        return List.of();
+        try (CSVReader csvReader = new CSVReader(new java.io.FileReader(filePath))) {
+            return csvReader.readAll()
+                    .stream()
+                    .map(line -> String.join(",", line))
+                    .collect(Collectors.toList());
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read file: " + filePath, e);
+        } catch (CsvException e) {
+            throw new RuntimeException("File is invalid: " + filePath, e);
+        }
     }
 }
