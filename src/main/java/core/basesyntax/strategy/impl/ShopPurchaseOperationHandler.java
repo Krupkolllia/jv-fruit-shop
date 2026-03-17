da80d277f30266f6000041bc39c7aec8dbcd9eab
Krupkolllia <krupkoilla397@gmail.com>
2026-03-17 23:33:08 +0100

    refactor: remove redundant null validations in ShopOperation Handlers

--- src/main/java/core/basesyntax/strategy/impl/ShopPurchaseOperationHandler.java
+++ src/main/java/core/basesyntax/strategy/impl/ShopPurchaseOperationHandler.java
@@ -6,10 +6,6 @@ import core.basesyntax.strategy.ShopOperationHandler;
 public class ShopPurchaseOperationHandler implements ShopOperationHandler {
     @Override
     public void handle(Storage storage, String product, Integer quantity) {
-        if (!storage.getAll().containsKey(product)) {
-            throw new RuntimeException("No such product as " + product);
-        }
-
         if (storage.getByKey(product) < quantity) {
             throw new RuntimeException("Not enough stock for product: " + product);
         }
--- src/main/java/core/basesyntax/strategy/impl/ShopSupplyOperationHandler.java
+++ src/main/java/core/basesyntax/strategy/impl/ShopSupplyOperationHandler.java
@@ -6,10 +6,6 @@ import core.basesyntax.strategy.ShopOperationHandler;
 public class ShopSupplyOperationHandler implements ShopOperationHandler {
     @Override
     public void handle(Storage storage, String product, Integer quantity) {
-        if (!storage.getAll().containsKey(product)) {
-            throw new RuntimeException("No such product as " + product);
-        }
-
         storage.update(product, storage.getByKey(product) + quantity);
     }
 }
