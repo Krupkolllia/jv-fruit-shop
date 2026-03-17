81456d5d5cd51595cad5c29a1a9f898bf9e93d55
Krupkolllia <krupkoilla397@gmail.com>
2026-03-17 02:32:27 +0100

    feat: implement ShopTransactionMapper

--- src/main/java/core/basesyntax/util/impl/ShopTransactionMapper.java
+++ src/main/java/core/basesyntax/util/impl/ShopTransactionMapper.java
@@ -6,6 +6,16 @@ import core.basesyntax.util.Mapper;
 public class ShopTransactionMapper implements Mapper<String, ShopTransaction> {
     @Override
     public ShopTransaction map(String from) {
-        return null;
+        String[] parts = from.split(",");
+        String stringOperation = parts[0];
+        ShopTransaction.Operation operation = switch (stringOperation) {
+            case "b" -> ShopTransaction.Operation.BALANCE;
+            case "s" -> ShopTransaction.Operation.SUPPLY;
+            case "p" -> ShopTransaction.Operation.PURCHASE;
+            case "r" -> ShopTransaction.Operation.RETURN;
+            default -> throw new RuntimeException("Unexpected operation: " + stringOperation);
+        };
+
+        return new ShopTransaction(operation, parts[1], Integer.parseInt(parts[2]));
     }
 }
