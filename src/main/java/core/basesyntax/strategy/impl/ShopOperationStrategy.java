7175845a6f11d280509b19124e15de4f6878eb0b
Krupkolllia <krupkoilla397@gmail.com>
2026-03-17 02:24:39 +0100

    feat: implement ShopOperationStrategy

--- src/main/java/core/basesyntax/strategy/impl/ShopOperationStrategy.java
+++ src/main/java/core/basesyntax/strategy/impl/ShopOperationStrategy.java
@@ -2,11 +2,20 @@ package core.basesyntax.strategy.impl;
 
 import core.basesyntax.model.ShopTransaction;
 import core.basesyntax.service.ShopOperationHandler;
+import core.basesyntax.service.impl.ShopBalanceOperationHandler;
+import core.basesyntax.service.impl.ShopPurchaseOperationHandler;
+import core.basesyntax.service.impl.ShopReturnOperationHandler;
+import core.basesyntax.service.impl.ShopSupplyOperationHandler;
 import core.basesyntax.strategy.OperationStrategy;
 
 public class ShopOperationStrategy implements OperationStrategy {
     @Override
     public ShopOperationHandler resolve(ShopTransaction.Operation operationType) {
-        return null;
+        return switch (operationType) {
+            case BALANCE -> new ShopBalanceOperationHandler();
+            case SUPPLY -> new ShopSupplyOperationHandler();
+            case PURCHASE -> new ShopPurchaseOperationHandler();
+            case RETURN -> new ShopReturnOperationHandler();
+        };
     }
 }
